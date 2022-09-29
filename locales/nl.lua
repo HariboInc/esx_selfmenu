fe581c8a1b7b0549f15d83f4b795e1fbfb682fb8
Raoul <50175388+HariboInc@users.noreply.github.com>
2022-09-29 22:50:45 +0200

    Add files via upload

---
+++ locales/nl.lua
@@ -0,0 +1,58 @@
+Locales['nl'] = {
+	-- Citizen Interactions
+	["citizen_interactions"] 	= "Burger Interactie",
+	["vehicle_menu"]			= "Voertuig menu",
+	["id_card"]					= "ID Kaart",
+	["id_card2"]				= "Rijbewijs",
+	["id_card3"]				= "Wapenvergunning",
+	["pee"]						= "plassen",
+	["poop"]					= "Poepen",
+	["blindfold"]				= "Blinddoek aandoen/uitdoen ",
+	["nobody_near"]				= "Geen speler in de buurt",
+	-- ID Menu
+	["check_id"]				= "Bekijk jouw ID",
+	["show_id"]					= "Laat je ID zien",
+	["check_id2"]				= "Bekijk jouw Rijbewijs",
+	["show_id2"]				= "Laat je Rijbewijs zien",
+	["check_id3"]				= "Bekijk jouw Wapenvergunning",
+	["show_id3"]				= "Laat je Wapenvergunning zien",
+	["id_menu"]					= "Maasgouw ID",
+	["dl_menu"]					= "Maasgouw Rijbewijs",
+	["gl_menu"]					= "Maasgouw Wapenvergunning",
+	-- Voertuigen Menu
+	["must_in_vehicle"]			= "Je moet in een voertuig zitten om dit menu te kunnen openen",
+	["engine"]					= "Motor",
+	["doors"]					= "Deuren",
+	["window"]					= "Ramen",
+	["cruise_control"]			= "Cruise Control",
+	["shuff"]					= "Verander van stoel",
+	["turned_off_engine"]		= "Motor uitgeschakeld",
+	["turned_on_engine"]		= "Motor aangezet",
+	-- Deuren Menu
+	["hood"]					= "Motorkap",
+	["trunk"]					= "Kofferbak",
+	["front_left_door"]			= "Linker voordeur",
+	["front_right_door"]		= "Rechter voordeur",
+	["back_left_door"]			= "Linker achterdeur",
+	["back_right_door"]			= "Rechter achterdeur",
+	["closed_hood"]				= "Motorkap gesloten!",
+	["opened_hood"]				= "Motorkap geopend!",
+	["closed_door"]				= "Deur geopend!",
+	["opened_door"]				= "Deur gesloten!",
+	["close_all"]				= "Alles sluiten",
+	["closed_all"]				= "Alle deuren gesloten!",
+	-- Window Menu
+	["window_menu"]				= "Ramen menu",
+	["front_left_window"]		= "Linker voorraam",
+	["front_right_window"]		= "Rechter voorraam",
+	["back_left_window"]		= "Linker achterraam",
+	["back_right_window"]		= "Rechter achterraam",
+	["opened_window"]			= "Je raam open gerold",
+	["closed_wondow"]			= "Je raam dicht gerold",
+	-- Cruise Control
+	["disabled"]				= "Uitschakelen",
+	["disabled_cruise_control"]	= "Cruise control uitgeschakeld",
+	-- Seatbelt
+	["seatbelt_on"]				= "Gordel vastgemaakt!",
+	["seatbelt_off"]			= "Gordel losgemaakt!"
+}
